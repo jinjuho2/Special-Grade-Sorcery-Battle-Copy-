8514f4a559b4cd7ef4c1d0135cf74f12b7725c15
jinjuho2 <jinjuho3@gmail.com>
2025-08-19 11:28:26 +0900

    Feat : 기초 UI 생성

---
+++ Assets/IDamageble.cs
@@ -0,0 +1,13 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+
+public interface IDamageble
+{
+    int health { get; set; }
+
+
+    void TakeDamage(int damage);
+    
+
+
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,31 @@
+﻿using Unity.VisualScripting;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class Player : MonoBehaviour , IDamageble
+{
+    public int health { get; set; } = 100; // Initial health of the player
+
+
+
+
+    public void TakeDamage(int amout)
+    {
+        if (health > 0)
+        {
+            health -= amout;
+            Debug.Log("Player took " + amout + " damage. Remaining health: " + health);
+        }
+        if (health <= 0)
+        {
+            Destroy(this.gameObject);
+            Time.timeScale = 0f; 
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Player has died.");
+        Time.timeScale = 0f; // 게임 일시정지
+    }
+}
